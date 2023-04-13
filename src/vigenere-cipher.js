157deb6295851214fbee741d520750e657c5238e
Mikita <anykey2solo@gmail.com>
2023-04-13 22:18:53 +0300

    feat: encoder

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,9 +20,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(text, key) {
+    let text2 = text.replaceAll(' ', '').toLowerCase();
+    let kf = Math.ceil(text.length / key.length);
+    key = key.repeat(kf).toLowerCase();
+   
+    let  codeA = 'a'.charCodeAt(0);
+    let abcCount = 26;
+    
+    let result = [];
+  let res = '';
+    for (let i = 0; i < text.length; i++) {
+      if(text2[i] === '!' || text2[i] === '1' || text2[i] === '2' || text2[i] === '3' || text2[i] === '4' || text2[i] === ',' || text2[i] === '.' || text2[i] === ':') {
+        result.push(text2[i])
+      } else {
+        let letterIndx = text2.charCodeAt(i) - codeA;
+        let shift = key.charCodeAt(i) - codeA;
+        result.push( String.fromCharCode(codeA + (letterIndx + shift) % abcCount));
+      }      
+    }
+  let control = result.join('').toUpperCase();
+   let c = 0;
+    for(let i = 0; i < text.length; i++) {
+      if(text[i] === ' ') {
+        res += " ";
+        c++;
+  
+      } else {
+        res += control[i - c]
+      }
+     
+    }
+  return res;
   }
   decrypt() {
     throw new NotImplementedError('Not implemented');
